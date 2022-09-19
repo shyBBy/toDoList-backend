f80e8104b03dcfeb3dd64907d766c1fa2615415f
Dawid Olczak <dawid.a.olczak@gmail.com>
2022-09-19 20:07:33 +0200

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import {config} from "./config/config";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+
+  const options = new DocumentBuilder()
+      .setTitle('TODO-LIST')
+      .setDescription('REST API DOCS')
+      .setVersion('1.0')
+      .addTag('todoList')
+      .addBearerAuth()
+      .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  // app.enableCors({
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  //   origin: config.corsFrontDomain,
+  // });
   await app.listen(3000);
 }
 bootstrap();
