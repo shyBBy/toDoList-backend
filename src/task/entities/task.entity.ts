34738f63b7e4eb1fcdbca83111da1c4a6d68f9df
Dawid Olczak <dawid.a.olczak@gmail.com>
2022-09-19 20:21:04 +0200

    added task entity

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,1 +1,34 @@
-export class Task extends{}
+import {BaseEntity, Column, PrimaryGeneratedColumn} from "typeorm";
+
+export class Task extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+
+    })
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column({
+        type: 'datetime',
+        default: null,
+    })
+    created_at: Date | string;
+
+    @Column()
+    user_id: string;
+
+    @Column({
+        type: 'datetime',
+        default: null,
+    })
+    expiration: Date | string;
+
+    @Column()
+    status: string;
+
+
+}
