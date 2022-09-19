dc4e17e82158cb6e3d0ec7286cd43ed295c6db13
Dawid Olczak <dawid.a.olczak@gmail.com>
2022-09-19 20:09:19 +0200

    added auth, user and task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
